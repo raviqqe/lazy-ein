77123e6ed3be79175b2f51e63c33ac5bb728ed86
Yota Toyama <raviqqe@gmail.com>
2018-10-12 05:57:04 +0000

    Fix linter error

--- ast/literal.go
+++ ast/literal.go
@@ -1,7 +1,14 @@
 package ast
 
-// Literal is a literal type.
+// Literal is a literal.
 type Literal interface {
 	Expression
 	isLiteral()
+}
+
+// IsLiteral checks if an expression is a literal or not.
+// This function is added only to fix the megacheck linter error.
+func IsLiteral(e Expression) bool {
+	_, ok := e.(Literal)
+	return ok
 }
