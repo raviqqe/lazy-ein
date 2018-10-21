373c86a6822e73e0bb82c5d993f111484b825729
Yota Toyama <raviqqe@gmail.com>
2018-10-21 22:10:24 +0000

    Rename environment pointer type

--- codegen/utilities.go
+++ codegen/utilities.go
@@ -3,7 +3,7 @@ package codegen
 import "llvm.org/llvm/bindings/go/llvm"
 
 var genericPointerType = llvm.PointerType(llvm.Int8Type(), 0)
-var thunkPointerType = genericPointerType
+var environmentPointerType = genericPointerType
 
 func toEntryName(s string) string {
 	return s + "-entry"
