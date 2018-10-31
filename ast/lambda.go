56e7e1f154a53add435f048eda43aa313d4e1e96
Yota Toyama <raviqqe@gmail.com>
2018-10-31 04:30:54 +0000

    Type free variables

--- ast/lambda.go
+++ ast/lambda.go
@@ -7,35 +7,23 @@ type Lambda struct {
 	arguments     []Argument
 	body          Expression
 	resultType    types.Type
-	freeVariables []string // TODO: Consider []ast.Variable.
+	freeVariables []Argument
 	updatable     bool
 }
 
 // NewLambda creates a new lambda form.
-func NewLambda(fs []string, u bool, as []Argument, e Expression, t types.Type) Lambda {
-	return Lambda{as, e, t, fs, u}
+func NewLambda(vs []Argument, u bool, as []Argument, e Expression, t types.Type) Lambda {
+	return Lambda{as, e, t, vs, u}
 }
 
 // ArgumentNames returns argument names.
 func (l Lambda) ArgumentNames() []string {
-	ss := make([]string, 0, len(l.arguments))
-
-	for _, a := range l.arguments {
-		ss = append(ss, a.Name())
-	}
-
-	return ss
+	return argumentsToNames(l.arguments)
 }
 
 // ArgumentTypes returns argument types.
 func (l Lambda) ArgumentTypes() []types.Type {
-	ts := make([]types.Type, 0, len(l.arguments))
-
-	for _, a := range l.arguments {
-		ts = append(ts, a.Type())
-	}
-
-	return ts
+	return argumentsToTypes(l.arguments)
 }
 
 // Body returns a body expression.
@@ -48,12 +36,37 @@ func (l Lambda) ResultType() types.Type {
 	return l.resultType
 }
 
-// FreeVariables returns a body expression.
-func (l Lambda) FreeVariables() []string {
-	return l.freeVariables
+// FreeVariableNames returns free varriable names.
+func (l Lambda) FreeVariableNames() []string {
+	return argumentsToNames(l.freeVariables)
+}
+
+// FreeVariableTypes returns free varriable types.
+func (l Lambda) FreeVariableTypes() []types.Type {
+	return argumentsToTypes(l.freeVariables)
 }
 
 // IsUpdatable returns true if the lambda form is updatable, or false otherwise.
 func (l Lambda) IsUpdatable() bool {
 	return l.updatable
+}
+
+func argumentsToNames(as []Argument) []string {
+	ss := make([]string, 0, len(as))
+
+	for _, a := range as {
+		ss = append(ss, a.Name())
+	}
+
+	return ss
+}
+
+func argumentsToTypes(as []Argument) []types.Type {
+	ts := make([]types.Type, 0, len(as))
+
+	for _, a := range as {
+		ts = append(ts, a.Type())
+	}
+
+	return ts
 }
