3cdfd2d9ce4309abac1a0d1272afaeaf8bfeef9f
Yota Toyama <raviqqe@gmail.com>
2018-11-17 21:43:06 +0000

    Add comment on updated-entry function

--- codegen/names/utilities.go
+++ codegen/names/utilities.go
@@ -6,6 +6,7 @@ func ToEntry(s string) string {
 }
 
 // ToUpdatedEntry converts a function name into its updated entry name.
+// TODO: Change this function name to "normal form entry".
 func ToUpdatedEntry(s string) string {
 	return s + ".updated-entry"
 }
