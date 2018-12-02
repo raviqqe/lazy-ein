5f60f0a5371a20026dae89229f02dcd0c37708bc
Yota Toyama <raviqqe@gmail.com>
2018-12-02 06:25:57 +0000

    Compile modules

---
+++ command/compile/compile.go
@@ -0,0 +1,31 @@
+package compile
+
+import (
+	"github.com/raviqqe/jsonxx/command/ast"
+	core "github.com/raviqqe/jsonxx/command/stg/ast"
+	"github.com/raviqqe/jsonxx/command/stg/types"
+)
+
+// Compile compiles a module into a module in STG.
+func Compile(m ast.Module) core.Module {
+	bs := make([]core.Bind, 0, len(m.Binds()))
+
+	for _, b := range m.Binds() {
+		bs = append(bs, compileBind(b))
+	}
+
+	return core.NewModule(m.Name(), nil, bs)
+}
+
+func compileBind(b ast.Bind) core.Bind {
+	return core.NewBind(
+		b.Name(),
+		core.NewLambda(
+			nil,
+			true,
+			nil,
+			core.NewFloat64(b.Expression().(ast.Number).Value()),
+			types.NewFloat64(),
+		),
+	)
+}
---
+++ command/compile/compile_test.go
@@ -0,0 +1,36 @@
+package compile_test
+
+import (
+	"testing"
+
+	"github.com/raviqqe/jsonxx/command/ast"
+	"github.com/raviqqe/jsonxx/command/compile"
+	core "github.com/raviqqe/jsonxx/command/stg/ast"
+	"github.com/raviqqe/jsonxx/command/stg/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompileWithEmptySource(t *testing.T) {
+	m := compile.Compile(ast.NewModule("", []ast.Bind{}))
+
+	assert.Equal(t, core.NewModule("", nil, []core.Bind{}), m)
+}
+
+func TestCompileWithVariableBinds(t *testing.T) {
+	m := compile.Compile(ast.NewModule("", []ast.Bind{ast.NewBind("x", ast.NewNumber(42))}))
+
+	assert.Equal(
+		t,
+		core.NewModule(
+			"",
+			nil,
+			[]core.Bind{
+				core.NewBind(
+					"x",
+					core.NewLambda(nil, true, nil, core.NewFloat64(42), types.NewFloat64()),
+				),
+			},
+		),
+		m,
+	)
+}
