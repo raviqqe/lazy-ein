29d527b2409c9a8242382e6186b419d3aea298df
Yota Toyama <raviqqe@gmail.com>
2018-12-02 11:53:32 +0000

    Generate object files

--- command/command.go
+++ command/command.go
@@ -2,9 +2,13 @@ package command
 
 import (
 	"io/ioutil"
+	"path/filepath"
+	"strings"
 
 	"github.com/raviqqe/jsonxx/command/compile"
+	core "github.com/raviqqe/jsonxx/command/core/compile"
 	"github.com/raviqqe/jsonxx/command/parse"
+	"llvm.org/llvm/bindings/go/llvm"
 )
 
 // Command runs a compiler command with command-line arguments.
@@ -27,9 +31,40 @@ func Command(ss []string) error {
 		return err
 	}
 
-	compile.Compile(m)
+	mm, err := core.Compile(compile.Compile(m))
 
-	// TODO: Generate object files.
+	if err != nil {
+		return err
+	}
 
-	return nil
+	llvm.InitializeAllTargets()
+	llvm.InitializeAllTargetInfos()
+	llvm.InitializeAllTargetMCs()
+	llvm.InitializeAllAsmParsers()
+	llvm.InitializeAllAsmPrinters()
+
+	t, err := llvm.GetTargetFromTriple(llvm.DefaultTargetTriple())
+
+	if err != nil {
+		return err
+	}
+
+	b, err := t.CreateTargetMachine(
+		mm.Target(),
+		"",
+		"",
+		llvm.CodeGenLevelDefault,
+		llvm.RelocDefault,
+		llvm.CodeModelDefault,
+	).EmitToMemoryBuffer(mm, llvm.ObjectFile)
+
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(
+		strings.TrimSuffix(as.Filename, filepath.Ext(as.Filename))+".o",
+		b.Bytes(),
+		0644,
+	)
 }
