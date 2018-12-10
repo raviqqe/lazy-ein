863bcca37753442953577f331e0bf361c563ca44
Yota Toyama <raviqqe@gmail.com>
2018-12-10 09:09:10 +0000

    Add error case test in generate package

--- command/generate/generate_test.go
+++ command/generate/generate_test.go
@@ -10,10 +10,46 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestModule(t *testing.T) {
+func TestExecutable(t *testing.T) {
 	os.Setenv("JSONXX_ROOT", "../..")
 
 	assert.Nil(
+		t,
+		generate.Executable(
+			"main.jsonxx",
+			ast.NewModule(
+				"main.jsonxx",
+				nil,
+				[]ast.Bind{
+					ast.NewBind(
+						"main",
+						ast.NewLambda(
+							nil,
+							false,
+							[]ast.Argument{ast.NewArgument("x", types.NewBoxed(types.NewFloat64()))},
+							ast.NewApplication(ast.NewVariable("x"), nil),
+							types.NewBoxed(types.NewFloat64()),
+						),
+					),
+				},
+			),
+		),
+	)
+}
+
+func TestExecutableErrorWithoutMainFunction(t *testing.T) {
+	os.Setenv("JSONXX_ROOT", "../..")
+
+	assert.Error(
+		t,
+		generate.Executable("main.jsonxx", ast.NewModule("main.jsonxx", nil, nil)),
+	)
+}
+
+func TestExecutableErrorWithoutRootEnvironmentVariable(t *testing.T) {
+	os.Unsetenv("JSONXX_ROOT")
+
+	assert.Error(
 		t,
 		generate.Executable(
 			"main.jsonxx",
