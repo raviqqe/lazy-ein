3c831c09623d84361ad15d9302f57472effb66f4
Yota Toyama <raviqqe@gmail.com>
2018-12-15 08:05:07 +0000

    Use debug error

--- command/types/error.go
+++ command/types/error.go
@@ -1,21 +1,7 @@
 package types
 
-import (
-	"fmt"
-
-	"github.com/ein-lang/ein/command/debug"
-)
-
-// TypeError is a type error.
-type TypeError struct {
-	message          string
-	debugInformation *debug.Information
-}
+import "github.com/ein-lang/ein/command/debug"
 
 func newTypeError(m string, i *debug.Information) error {
-	return TypeError{m, i}
-}
-
-func (e TypeError) Error() string {
-	return fmt.Sprintf("TypeError: %v\n%v", e.message, e.debugInformation)
+	return debug.NewError("TypeError", m, i)
 }
--- command/types/error_test.go
+++ command/types/error_test.go
@@ -4,16 +4,8 @@ import (
 	"testing"
 
 	"github.com/ein-lang/ein/command/debug"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestTypeErrorError(t *testing.T) {
-	assert.Equal(
-		t,
-		"TypeError: you are wrong\nfoo.go:42:2049:\tfunc foo() {}",
-		newTypeError(
-			"you are wrong",
-			debug.NewInformation("foo.go", 42, 2049, "func foo() {}"),
-		).Error(),
-	)
+func TestNewTypeError(t *testing.T) {
+	newTypeError("you are wrong", debug.NewInformation("foo.go", 42, 2049, "func foo() {}"))
 }
