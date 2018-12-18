f07d8305eb6492a2d8e8ae3c402311db7d8a05cc
Yota Toyama <raviqqe@gmail.com>
2018-12-18 03:21:17 +0000

    Add lambda forms in AST

---
+++ command/ast/lambda.go
@@ -0,0 +1,29 @@
+package ast
+
+// Lambda is a lambda.
+type Lambda struct {
+	arguments  []string
+	expression Expression
+}
+
+// NewLambda creates a lambda.
+func NewLambda(as []string, e Expression) Lambda {
+	return Lambda{as, e}
+}
+
+// Arguments returns arguments.
+func (b Lambda) Arguments() []string {
+	return b.arguments
+}
+
+// Expression returns an expression.
+func (b Lambda) Expression() Expression {
+	return b.expression
+}
+
+// ConvertExpression visits expressions.
+func (b Lambda) ConvertExpression(f func(Expression) Expression) node {
+	return NewLambda(b.arguments, f(b.expression.ConvertExpression(f).(Expression)))
+}
+
+func (Lambda) isExpression() {}
