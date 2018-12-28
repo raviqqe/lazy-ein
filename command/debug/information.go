22fc5b23cd256544a95b3f3cd191b9aeb43c01be
Yota Toyama <raviqqe@gmail.com>
2018-12-28 06:16:45 +0000

    Rename some fields of debug.Information

--- command/debug/information.go
+++ command/debug/information.go
@@ -4,16 +4,16 @@ import "fmt"
 
 // Information is debug information.
 type Information struct {
-	filename       string
-	line, position int
-	code           string
+	filename     string
+	line, column int
+	source       string
 }
 
 // NewInformation creates debug information.
-func NewInformation(f string, l, p int, c string) *Information {
-	return &Information{f, l, p, c}
+func NewInformation(f string, l, c int, s string) *Information {
+	return &Information{f, l, c, s}
 }
 
 func (i *Information) String() string {
-	return fmt.Sprintf("%s:%d:%d:\t%s", i.filename, i.line, i.position, i.code)
+	return fmt.Sprintf("%s:%d:%d:\t%s", i.filename, i.line, i.column, i.source)
 }
