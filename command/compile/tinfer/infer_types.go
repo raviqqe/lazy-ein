43c8f28c3271f1e8866cdd7f456c0f80304c4d93
Yota Toyama <raviqqe@gmail.com>
2019-01-06 09:26:04 +0000

    Validate resulting modules after type inference

--- command/compile/tinfer/infer_types.go
+++ command/compile/tinfer/infer_types.go
@@ -1,8 +1,30 @@
 package tinfer
 
-import "github.com/ein-lang/ein/command/ast"
+import (
+	"github.com/ein-lang/ein/command/ast"
+	"github.com/ein-lang/ein/command/debug"
+	"github.com/ein-lang/ein/command/types"
+)
 
 // InferTypes infers types in a module.
 func InferTypes(m ast.Module) (ast.Module, error) {
-	return newInferrer(m).Infer(m)
+	m, err := newInferrer(m).Infer(m)
+
+	if err != nil {
+		return ast.Module{}, err
+	} else if err := validateFullyTypedModule(m); err != nil {
+		return ast.Module{}, err
+	}
+
+	return m, nil
+}
+
+func validateFullyTypedModule(m ast.Module) error {
+	return m.VisitTypes(func(t types.Type) error {
+		if v, ok := t.(types.Variable); ok {
+			return debug.NewError("TypeInferenceError", "failed to infer a type", v.DebugInformation())
+		}
+
+		return nil
+	})
 }
