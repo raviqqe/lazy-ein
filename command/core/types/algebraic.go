a15e9252c1cd93ceb6530903bb18093dae362c11
Yota Toyama <raviqqe@gmail.com>
2019-01-09 04:53:09 +0000

    Add named algebraic and function types

--- command/core/types/algebraic.go
+++ command/core/types/algebraic.go
@@ -2,12 +2,27 @@ package types
 
 // Algebraic is an algebraic type.
 type Algebraic struct {
+	name         string
 	constructors []Constructor
 }
 
 // NewAlgebraic creates an algebraic type.
 func NewAlgebraic(cs []Constructor) Algebraic {
-	return Algebraic{cs}
+	return Algebraic{"", cs}
+}
+
+// NewNamedAlgebraic creates an algebraic type.
+func NewNamedAlgebraic(n string, cs []Constructor) Algebraic {
+	return Algebraic{n, cs}
+}
+
+// Name returns a name.
+func (a Algebraic) Name() (string, bool) {
+	if a.name == "" {
+		return "", false
+	}
+
+	return a.name, true
 }
 
 // Constructors returns constructors.
--- command/core/types/function.go
+++ command/core/types/function.go
@@ -2,13 +2,28 @@ package types
 
 // Function is a function type.
 type Function struct {
+	name      string
 	arguments []Type
 	result    Type
 }
 
 // NewFunction creates a function type.
 func NewFunction(as []Type, r Type) Function {
-	return Function{as, r}
+	return Function{"", as, r}
+}
+
+// NewNamedFunction creates a function type.
+func NewNamedFunction(n string, as []Type, r Type) Function {
+	return Function{n, as, r}
+}
+
+// Name returns a name.
+func (f Function) Name() (string, bool) {
+	if f.name == "" {
+		return "", false
+	}
+
+	return f.name, true
 }
 
 // Arguments returns arguments.
