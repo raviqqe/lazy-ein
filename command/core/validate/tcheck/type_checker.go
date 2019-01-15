a160f34d2d52c8fd483087d3838f9b0169733add
Yota Toyama <raviqqe@gmail.com>
2019-01-15 06:49:08 +0000

    Refactor type checker

--- command/core/validate/tcheck/type_checker.go
+++ command/core/validate/tcheck/type_checker.go
@@ -47,155 +47,179 @@ func (c typeChecker) checkLambda(l ast.Lambda) error {
 func (c typeChecker) checkExpression(e ast.Expression) (types.Type, error) {
 	switch e := e.(type) {
 	case ast.AlgebraicCase:
-		t, err := c.checkExpression(e.Argument())
-
-		if err != nil {
-			return nil, err
-		} else if len(e.Alternatives()) == 0 {
-			d, _ := e.DefaultAlternative()
-			return c.checkDefaultAlternative(d, t)
-		}
-
-		tt, err := c.checkAlgebraicAlternative(e.Alternatives()[0])
-
-		if err != nil {
-			return nil, err
-		}
-
-		for _, a := range e.Alternatives()[1:] {
-			if err := c.checkTypes(a.Constructor().AlgebraicType(), types.Unbox(t)); err != nil {
-				return nil, err
-			}
-
-			if ttt, err := c.checkAlgebraicAlternative(a); err != nil {
-				return nil, err
-			} else if err := c.checkTypes(ttt, tt); err != nil {
-				return nil, err
-			}
-		}
-
-		d, ok := e.DefaultAlternative()
-
-		if !ok {
-			return tt, nil
-		}
-
-		return c.checkDefaultAlternative(d, t)
+		return c.checkAlgebraicCase(e)
 	case ast.Atom:
-		return c.inferAtomType(e)
+		return c.checkAtom(e)
 	case ast.ConstructorApplication:
-		if len(e.Arguments()) != len(e.Constructor().ConstructorType().Elements()) {
-			return nil, errors.New("invalid number of constructor arguments")
-		}
-
-		for i, a := range e.Arguments() {
-			t, err := c.inferAtomType(a)
-
-			if err != nil {
-				return nil, err
-			}
-
-			tt := e.Constructor().ConstructorType().Elements()[i]
-
-			if err := c.checkTypes(t, tt); err != nil {
-				return nil, err
-			}
-		}
-
-		return e.Constructor().AlgebraicType(), nil
+		return c.checkConstructorApplication(e)
 	case ast.FunctionApplication:
-		if len(e.Arguments()) == 0 {
-			return c.getVariableType(e.Function())
-		}
-
-		t, err := c.getVariableType(e.Function())
-
-		if err != nil {
-			return nil, err
-		}
-
-		f, ok := t.(types.Function)
-
-		if !ok {
-			return nil, errors.New("not a function")
-		} else if len(f.Arguments()) != len(e.Arguments()) {
-			return nil, errors.New("invalid number of function arguments")
-		}
-
-		for i, t := range f.Arguments() {
-			tt, err := c.inferAtomType(e.Arguments()[i])
-
-			if err != nil {
-				return nil, err
-			} else if err := c.checkTypes(t, tt); err != nil {
-				return nil, err
-			}
-		}
-
-		return f.Result(), nil
+		return c.checkFunctionApplication(e)
 	case ast.Let:
-		c, err := c.checkBinds(e.Binds())
-
-		if err != nil {
-			return nil, err
-		}
-
-		return c.checkExpression(e.Expression())
+		return c.checkLet(e)
 	case ast.PrimitiveCase:
-		if t, err := c.checkExpression(e.Argument()); err != nil {
-			return nil, err
-		} else if err := c.checkTypes(t, e.Type()); err != nil {
-			return nil, err
-		} else if d, ok := e.DefaultAlternative(); ok && len(e.Alternatives()) == 0 {
-			return c.checkDefaultAlternative(d, e.Type())
-		}
-
-		t, err := c.checkExpression(e.Alternatives()[0].Expression())
-
-		if err != nil {
-			return nil, err
-		}
-
-		for _, a := range e.Alternatives()[1:] {
-			if err := c.checkTypes(c.getLiteralType(a.Literal()), types.Unbox(e.Type())); err != nil {
-				return nil, err
-			}
-
-			if tt, err := c.checkExpression(a.Expression()); err != nil {
-				return nil, err
-			} else if err := c.checkTypes(t, tt); err != nil {
-				return nil, err
-			}
-		}
-
-		d, ok := e.DefaultAlternative()
-
-		if !ok {
-			return t, nil
-		}
-
-		return c.checkDefaultAlternative(d, e.Type())
+		return c.checkPrimitiveCase(e)
 	case ast.PrimitiveOperation:
-		for _, a := range e.Arguments() {
-			t, err := c.inferAtomType(a)
-
-			if err != nil {
-				return nil, err
-			} else if err := c.checkTypes(t, types.NewFloat64()); err != nil {
-				return nil, err
-			}
-		}
-
-		return types.NewFloat64(), nil
+		return c.checkPrimitiveOperation(e)
 	}
 
 	panic("unreachable")
+}
+
+func (c typeChecker) checkAlgebraicCase(ac ast.AlgebraicCase) (types.Type, error) {
+	t, err := c.checkExpression(ac.Argument())
+
+	if err != nil {
+		return nil, err
+	} else if len(ac.Alternatives()) == 0 {
+		d, _ := ac.DefaultAlternative()
+		return c.checkDefaultAlternative(d, t)
+	}
+
+	tt, err := c.checkAlgebraicAlternative(ac.Alternatives()[0])
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range ac.Alternatives()[1:] {
+		if err := c.checkTypes(a.Constructor().AlgebraicType(), types.Unbox(t)); err != nil {
+			return nil, err
+		}
+
+		if ttt, err := c.checkAlgebraicAlternative(a); err != nil {
+			return nil, err
+		} else if err := c.checkTypes(ttt, tt); err != nil {
+			return nil, err
+		}
+	}
+
+	d, ok := ac.DefaultAlternative()
+
+	if !ok {
+		return tt, nil
+	}
+
+	return c.checkDefaultAlternative(d, t)
 }
 
 func (c typeChecker) checkAlgebraicAlternative(a ast.AlgebraicAlternative) (types.Type, error) {
 	return c.addConstructorElements(a.Constructor(), a.ElementNames()).checkExpression(
 		a.Expression(),
 	)
+}
+
+func (c typeChecker) checkConstructorApplication(a ast.ConstructorApplication) (types.Type, error) {
+	if len(a.Arguments()) != len(a.Constructor().ConstructorType().Elements()) {
+		return nil, errors.New("invalid number of constructor arguments")
+	}
+
+	for i, arg := range a.Arguments() {
+		t, err := c.checkAtom(arg)
+
+		if err != nil {
+			return nil, err
+		}
+
+		tt := a.Constructor().ConstructorType().Elements()[i]
+
+		if err := c.checkTypes(t, tt); err != nil {
+			return nil, err
+		}
+	}
+
+	return a.Constructor().AlgebraicType(), nil
+}
+
+func (c typeChecker) checkFunctionApplication(a ast.FunctionApplication) (types.Type, error) {
+	if len(a.Arguments()) == 0 {
+		return c.checkVariable(a.Function())
+	}
+
+	t, err := c.checkVariable(a.Function())
+
+	if err != nil {
+		return nil, err
+	}
+
+	f, ok := t.(types.Function)
+
+	if !ok {
+		return nil, errors.New("not a function")
+	} else if len(f.Arguments()) != len(a.Arguments()) {
+		return nil, errors.New("invalid number of function arguments")
+	}
+
+	for i, t := range f.Arguments() {
+		tt, err := c.checkAtom(a.Arguments()[i])
+
+		if err != nil {
+			return nil, err
+		} else if err := c.checkTypes(t, tt); err != nil {
+			return nil, err
+		}
+	}
+
+	return f.Result(), nil
+}
+
+func (c typeChecker) checkLet(l ast.Let) (types.Type, error) {
+	c, err := c.checkBinds(l.Binds())
+
+	if err != nil {
+		return nil, err
+	}
+
+	return c.checkExpression(l.Expression())
+}
+
+func (c typeChecker) checkPrimitiveCase(pc ast.PrimitiveCase) (types.Type, error) {
+	if t, err := c.checkExpression(pc.Argument()); err != nil {
+		return nil, err
+	} else if err := c.checkTypes(t, pc.Type()); err != nil {
+		return nil, err
+	} else if d, ok := pc.DefaultAlternative(); ok && len(pc.Alternatives()) == 0 {
+		return c.checkDefaultAlternative(d, pc.Type())
+	}
+
+	t, err := c.checkExpression(pc.Alternatives()[0].Expression())
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range pc.Alternatives()[1:] {
+		if err := c.checkTypes(c.getLiteralType(a.Literal()), types.Unbox(pc.Type())); err != nil {
+			return nil, err
+		}
+
+		if tt, err := c.checkExpression(a.Expression()); err != nil {
+			return nil, err
+		} else if err := c.checkTypes(t, tt); err != nil {
+			return nil, err
+		}
+	}
+
+	d, ok := pc.DefaultAlternative()
+
+	if !ok {
+		return t, nil
+	}
+
+	return c.checkDefaultAlternative(d, pc.Type())
+}
+
+func (c typeChecker) checkPrimitiveOperation(o ast.PrimitiveOperation) (types.Type, error) {
+	for _, a := range o.Arguments() {
+		t, err := c.checkAtom(a)
+
+		if err != nil {
+			return nil, err
+		} else if err := c.checkTypes(t, types.NewFloat64()); err != nil {
+			return nil, err
+		}
+	}
+
+	return types.NewFloat64(), nil
 }
 
 func (c typeChecker) checkDefaultAlternative(d ast.DefaultAlternative, t types.Type) (types.Type, error) {
@@ -210,12 +234,12 @@ func (typeChecker) checkTypes(t, tt types.Type) error {
 	return nil
 }
 
-func (c typeChecker) inferAtomType(a ast.Atom) (types.Type, error) {
+func (c typeChecker) checkAtom(a ast.Atom) (types.Type, error) {
 	switch a := a.(type) {
 	case ast.Literal:
 		return c.getLiteralType(a), nil
 	case ast.Variable:
-		return c.getVariableType(a)
+		return c.checkVariable(a)
 	}
 
 	panic("unreachable")
@@ -230,7 +254,7 @@ func (c typeChecker) getLiteralType(l ast.Literal) types.Type {
 	panic("unreachable")
 }
 
-func (c typeChecker) getVariableType(v ast.Variable) (types.Type, error) {
+func (c typeChecker) checkVariable(v ast.Variable) (types.Type, error) {
 	t, ok := c.variables[v.Name()]
 
 	if !ok {
