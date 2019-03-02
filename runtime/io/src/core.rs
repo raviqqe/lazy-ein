7d322e2b8a45187af45138f2528762d410f287de
Yota Toyama <raviqqe@gmail.com>
2019-03-02 09:42:14 +0000

    Add list type

--- runtime/io/src/core.rs
+++ runtime/io/src/core.rs
@@ -14,31 +14,68 @@ macro_rules! closure {
     };
 }
 
+#[derive(Clone, Copy)]
 #[repr(C)]
 pub struct Closure<E, P> {
     pub entry: E,
     pub payload: P,
 }
 
+impl<E, P> Closure<E, P> {
+    pub fn new(entry: E, payload: P) -> Self {
+        Closure { entry, payload }
+    }
+}
+
+impl<T: Clone> From<&[T]> for List<T> {
+    fn from(xs: &[T]) -> List<T> {
+        Closure::new(list_entry, xs.into())
+    }
+}
+
 impl From<f64> for Number {
     fn from(n: f64) -> Number {
-        Closure {
-            entry: number_entry,
-            payload: n.into(),
-        }
+        Closure::new(number_entry, n.into())
     }
 }
 
 #[repr(C)]
 pub struct Environment(i8); // avoid zero-sized type for compatibility with C
 
+pub type List<T> = closure!(algebraic::List<T>);
 pub type Number = closure!(algebraic::Number);
+
+extern "fastcall" fn list_entry<T: Clone>(list: &mut algebraic::List<T>) -> algebraic::List<T> {
+    list.clone()
+}
 
 extern "fastcall" fn number_entry(number: &mut algebraic::Number) -> algebraic::Number {
     *number
 }
 
 pub mod algebraic {
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub enum List<T: Clone> {
+        Cons(T, *mut super::List<T>),
+        Nil,
+    }
+
+    impl<T: Clone, S: Clone + Into<T>> From<&[S]> for List<T> {
+        fn from(xs: &[S]) -> List<T> {
+            let mut l = List::Nil;
+
+            for x in xs.into_iter().rev() {
+                l = List::Cons(
+                    x.clone().into(),
+                    &mut super::Closure::new(super::list_entry, l),
+                )
+            }
+
+            l
+        }
+    }
+
     #[derive(Clone, Copy)]
     #[repr(C)]
     pub struct Number(f64);
